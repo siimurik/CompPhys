68ee2182793d4562707f0be825d7bf2b365f2ded
siimurik <82040815+siimurik@users.noreply.github.com>
2022-08-29 20:13:12 +0300

    Update run_step_func.sh

--- Adv_C_Fortran/C_Advanced/Cheb_Approx/run_step_func.sh
+++ Adv_C_Fortran/C_Advanced/Cheb_Approx/run_step_func.sh
@@ -1,7 +1,7 @@
 echo Compiling the main code.
 gcc -Wall -I/home/siim/gsl/include -c step_func_Cheb.c 
 gcc -L/home/siim/gsl/lib step_func_Cheb.o -lgsl -lgslcblas -lm
-export LD_LIBRARY_PATH="/home/siim/gsl/lib"
+export LD_LIBRARY_PATH="/home/kde_siimep/gsl/lib"
 echo Executing and printing the results into the 'data.dat' file.
 ./a.out > data.dat
 echo Plotting the results in Python.
