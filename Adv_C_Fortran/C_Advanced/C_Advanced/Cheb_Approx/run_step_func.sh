0935727e2430660b75c405d2d0cadbe499676604
siimurik <82040815+siimurik@users.noreply.github.com>
2022-12-06 22:13:08 +0200

    Delete Adv_C_Fortran/C_Advanced/C_Advanced directory

--- Adv_C_Fortran/C_Advanced/C_Advanced/Cheb_Approx/run_step_func.sh
+++
@@ -1,6 +0,0 @@
-echo Compiling the main code.
-gcc -I/usr/include/gsl -lgsl -lgslcblas -lm step_func_Cheb.c -o step
-echo Executing and printing the results into the 'data.dat' file.
-./step > data.dat
-echo Plotting the results in Python.
-python3 plot_step_func.py
