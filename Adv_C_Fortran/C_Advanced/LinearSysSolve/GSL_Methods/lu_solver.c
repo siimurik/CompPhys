a1ebc0261799034055c83900ff0e19edb03bbb78
siimurik <82040815+siimurik@users.noreply.github.com>
2023-01-24 18:32:35 +0200

    Update lu_solver.c

--- Adv_C_Fortran/C_Advanced/LinearSysSolve/GSL_Methods/lu_solver.c
+++ Adv_C_Fortran/C_Advanced/LinearSysSolve/GSL_Methods/lu_solver.c
@@ -1,95 +1,83 @@
 /*
 ========================================================
  Compile and execute with:
-    $ gcc -o lus lu_solver.c  -lgsl -lgslcblas
+    $ gcc -o lus lu_solver.c -lgsl -lgslcblas
     $ ./lus
+========================================================
+ This is a linear system solver code that uses GSL's 
+ LU-decomposition and then solves the system of linear
+ equations using the decomposed matrix. This code works
+ most effectively for small matrices.
 ========================================================
 */
 #include <stdio.h>
 #include <time.h>
 #include <gsl/gsl_linalg.h>
+
 #define n 201
 int main(void)
-{
-    // Matrix A has dimensions MxN
-    int N = n-1;
+{    
+    // Create the matrix
+    gsl_matrix *A = gsl_matrix_alloc (n+1, n+1);
+    gsl_vector *x = gsl_vector_alloc (n+1);
+    gsl_vector *y = gsl_vector_alloc (n+1);
+    int s;
+    double h = 2.0/(double)(n-1);
+    static double X[n+1];
 
-    // Declare arrays to hold the matrix A and vector B
-    double h = 2.0/(double)(N);
-    static double A[n*n];
-    static double X[n];
-    static double y[n];
-
+    // A vector of 200 elements spanning
+    // from 0.0 to 2.0
     for (int i = 0; i < n; i++)
     {
         X[i] = i*h;
     }
-    //A[0][0] = 1.0/h + h/2*(4-X[0]);
-    //A[0][1] = -1.0/h;
-    A[0] = 1.0/h + h/2*(4-X[0]);
-    A[1] = -1.0/h;
-    y[0] = h/2*(X[0]+5) - 1;
 
-    for (int i = 1; i < N; i++)
+    // Setting left boundary conditions
+    gsl_matrix_set(A, 0, 0, 1.0/h + h/2*(4-X[0]));
+    gsl_matrix_set(A, 0, 1, -1.0/h);
+    gsl_vector_set(y, 0, h/2*(X[0]+5) - 1) ;
+
+    // Writing the diagonal elements
+    for (int i = 1; i < n; i++)
     {
-        //A[i][i-1] = -1.0/h;
-        //A[i][i]   =  2.0/h + h*(4-X[i]);
-        //A[i][i+1] = -1.0/h;
-        A[i*n + i-1] = -1.0/h;
-        A[i*n + i]   =  2.0/h + h*(4-X[i]);
-        A[i*n + i+1] = -1.0/h;
-        y[i]     = h*(X[i]+5);
+        gsl_matrix_set(A, i, i-1, -1.0/h);
+        gsl_matrix_set(A, i, i, 2.0/h + h*(4-X[i]));
+        gsl_matrix_set(A, i, i+1, -1.0/h);
+        gsl_vector_set(y, i, h*(X[i]+5));
     }
 
-    //A[N][N-1]   = -1.0/h;
-    //A[N][N] = 1.0/h + h/2*(4-X[N]);
-    A[N*n + N-1]   = -1.0/h;
-    A[N*n + N] = 1.0/h + h/2*(4-X[N]);
-    y[N]     = h/2*(X[N]+5) - 1;
+    // Setting right boundary conditions
+    gsl_matrix_set(A, n, n-1, -1.0/h);
+    gsl_matrix_set(A, n, n, 1.0/h + h/2*(4-X[n]));
+    gsl_vector_set(y, n, h/2*(X[n]+5) - 1);
 
-/*
-    // Set the values of A and B
-    // by writing matrix values into vectors
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            AA[i*n + j] = A[i][j];
-        }
-        B[i] = y[i];
-    }
-*/
-
-    // Print out the values of A
-    printf("\nLower right elements of matrix A:\n");
+    // Print the initial matrix
+    printf("Initial Matrix\n");
     for (int i = n-5; i < n; i++)
-    {
-        for (int j = n-5; j < n; j++)
         {
-            //printf("%lf ", A[i][j]);
-            printf("%lf ", A[i*n + j]);
-
-        }
+        for (int j = n-5; j < n; j++)
+            {
+                printf("%16.4f ",gsl_matrix_get(A, i, j));
+            }
         printf("\n");
-    }
+        }
+    printf("\n");
+
+    // Print out vectors
     printf("\nX:\t\ty:\n");
     for (int i = n-5; i < n; i++)
     {
-        printf("%lf\t%lf\n ", X[i], y[i]);
+        printf("%lf\t%lf\n ", X[i], gsl_vector_get(y,i));
     }
 
-
-    gsl_matrix_view m = gsl_matrix_view_array (A, n, n);
-    gsl_vector_view b = gsl_vector_view_array (y, n);
-    gsl_vector *x = gsl_vector_alloc (n);
-
-    int s;
-
-    gsl_permutation * p = gsl_permutation_alloc (n);
-    gsl_linalg_LU_decomp (&m.matrix, p, &s);
+    // Perform LU-decomposition
+    gsl_permutation * p = gsl_permutation_alloc (n+1);
+    gsl_linalg_LU_decomp (A, p, &s);
 
     // Get starting time
     clock_t start = clock();
 
-    gsl_linalg_LU_solve (&m.matrix, p, &b.vector, x);
+    gsl_linalg_LU_solve (A, p, y, x);
 
     // Get end time
     clock_t stop = clock();
@@ -106,7 +94,7 @@ int main(void)
     // Print out first five elements of the solution
     printf ("\nSolution: \ni \t u \n");
     //gsl_vector_fprintf (stdout, x, "%g");
-    for (int i = 0; i < 5; i++)
+    for (int i = n-5; i < n; i++)
         //printf ("element %d = %g\n", i, gsl_vector_get (x, i));
         printf ("%d \t %g\n", i, gsl_vector_get (x, i));
 
@@ -114,8 +102,11 @@ int main(void)
     double elapsed_time = (double)(stop - start) / CLOCKS_PER_SEC;
     printf("\nElapsed time: %g sec.\n", elapsed_time);
 
+    // Free allocated memory
     gsl_permutation_free (p);
     gsl_vector_free (x);
+    gsl_vector_free (y);
+    gsl_matrix_free (A);
 
     return 0;
 }
