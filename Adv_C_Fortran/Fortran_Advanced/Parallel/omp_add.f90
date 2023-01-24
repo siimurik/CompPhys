f50a71f0244e8b497a94034efcad6b7eb5e88e3a
siimurik <82040815+siimurik@users.noreply.github.com>
2023-01-24 17:13:31 +0200

    Add files via upload

---
+++ Adv_C_Fortran/Fortran_Advanced/Parallel/omp_add.f90
@@ -0,0 +1,38 @@
+!===================================================
+! Compile and execute with:
+!   $ gfortran -fopenmp omp_add.f90
+!   $ time OMP_NUM_THREADS=3 ./a.out
+!===================================================
+program omp_vec_addition
+
+    integer n, chunksize, chunk, i
+    parameter (n=10000)
+    parameter (chunksize=100)
+    real a(n), b(n), c(n)
+
+    !initialization of vectors
+    do i = 1, n
+        a(i) = i
+        b(i) = a(i) * n
+    enddo
+    chunk = chunksize
+
+    !$omp parallel shared(a,b,c,chunk) private(i)
+
+    !$omp do schedule(dynamic,chunk)
+    do i = 1, n
+        c(i) = a(i) + b(i)
+    enddo
+    !$omp end do
+
+    !$omp end parallel
+
+    do i = 1, 10
+        write(*,'(3F17.1)') a(i), b(i), c(i)
+    end do
+    write(*,*) '...'
+    do i = 1, 10
+        write(*,'(3F17.1)') a(n-10+i), b(n-10+i), c(n-10+i)
+    end do
+
+end program omp_vec_addition
