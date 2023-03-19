260326f054369f24304336aea67d5089a891afd2
siimurik <82040815+siimurik@users.noreply.github.com>
2023-03-19 14:22:10 +0200

    Update sum_check.c

--- Adv_C_Fortran/C_Advanced/Series/sum_check.c
+++ Adv_C_Fortran/C_Advanced/Series/sum_check.c
@@ -9,17 +9,16 @@
 
 int main()
 {
-    double sum = 0.0, term = 1.0, tol = 1e-10;
-    int k = 4;
+    double sum = 0.0, tol = 1e-10;
+    int k = 1;
     clock_t start_time = clock(); // start the timer
     double elapsed_time = 0.0;    // initialize the elapsed time to zero
-    while (fabs(term) > tol) {
+    double term;
+    do {
+        term = sin(2.0*k)/(k + pow(3.0, k));
+        //term = 3.0*k/((k*k-2.0)*(log(k)));
         sum += term;
         k++;
-        //term = 1.0 / log(k); // compute the next term
-        term = pow(2, k) / tgamma(k+1.0); // converges quite nicely
-        // term = 1.0/( k*log(k)*pow(log(log(k)), 0.5) );
-
         elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC; // update elapsed time
         if (elapsed_time >= 5) { // stop if elapsed time is >= 5 seconds
             printf("\rElapsed time: %.2f seconds", elapsed_time); // print elapsed time without newline
@@ -30,7 +29,7 @@ int main()
         }
         printf("\rElapsed time: %.2f seconds", elapsed_time); // print elapsed time without newline
         fflush(stdout); // flush stdout to force printing
-    }
+    } while (fabs(term) > tol);
     printf("\nsum = %.16f\n", sum);
     printf("k = %i\n", k);
     return 0;
