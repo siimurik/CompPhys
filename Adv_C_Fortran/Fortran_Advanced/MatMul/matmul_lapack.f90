12ffd2ab5be7fef9ba9e0cc6d6aba8bf2fade2b4
siimurik <82040815+siimurik@users.noreply.github.com>
2023-08-13 23:12:32 +0300

    Add files via upload

--- Adv_C_Fortran/Fortran_Advanced/MatMul/matmul_lapack.f90
+++ Adv_C_Fortran/Fortran_Advanced/MatMul/matmul_lapack.f90
@@ -1,21 +1,28 @@
-!* Fortran source code is found in dgemm_example.f
+!---------------------------------------------------------------------------------
 ! Compile and execute with:
 !   $ gfortran -o matmul_lapack matmul_lapack.f90 -L/usr/local/lib -llapack -lblas
 ! Or
 !   $ ifx -o matmul_lapack matmul_lapack.f90 -llapack -lblas
 !   $ ./matmul_lapack
-!
+!---------------------------------------------------------------------------------
+! For multithreading uncomment the line
+!   CALL OPENBLAS_SET_NUM_THREADS(8)
+! and compile with
+!   $ gfortran matmul_lapack.f90 -o matmul_lapack -lopenblas -lpthread
+!---------------------------------------------------------------------------------
 PROGRAM   MAIN
 
     IMPLICIT NONE
 
-    DOUBLE PRECISION                 :: ALPHA, BETA, T1, T2, TIME
-    INTEGER              :: I, J
+    DOUBLE PRECISION    :: ALPHA, BETA
+    INTEGER             :: I, J
     !INTEGER(4), PARAMETER   :: M=2000, K=200, N=1000
-    INTEGER, PARAMETER   :: M=5000, K=5000, N=5000
+    INTEGER, PARAMETER  :: M=5000, K=5000, N=5000
     DOUBLE PRECISION, DIMENSION(M,K) :: A
     DOUBLE PRECISION, DIMENSION(K,N) :: B
     DOUBLE PRECISION, DIMENSION(M,N) :: C
+    INTEGER :: START_TIME, END_TIME, ELAPSED_TIME, RATE
+    REAL :: ELAPSED_SECONDS
 
     PRINT *, "This example computes real matrix C=alpha*A*B+beta*C"
     PRINT *, "using LAPACK package function dgemm, where A, B, and C"
@@ -48,25 +55,29 @@ PROGRAM   MAIN
 
     CALL RANDOM_NUMBER(B)
 
-    !DO I = 1, M
-    !    DO J = 1, N
-    !        C(I,J) = 0.0
-    !    END DO
-    !END DO
-
     C = 0.0
+    
+    ! Specify the number of cores to be used
+    CALL OPENBLAS_SET_NUM_THREADS(8)
 
     PRINT *, "Computing matrix product using LAPACK DGEMM "
     PRINT *, "subroutine"
-    CALL CPU_TIME(T1)
+
+    CALL SYSTEM_CLOCK(count=START_TIME, count_rate=RATE)
+
     CALL DGEMM('N','N',M,N,K,ALPHA,A,M,B,K,BETA,C,M)
-    CALL CPU_TIME(T2)
-    TIME = T2 - T1
+    
+    CALL SYSTEM_CLOCK(count=END_TIME)
+
+    ELAPSED_TIME = END_TIME - START_TIME
+    ELAPSED_SECONDS = REAL(ELAPSED_TIME) / REAL(RATE)
+
     PRINT *, "Computations completed."
-    WRITE (*,15) TIME
+    WRITE (*,15) ELAPSED_SECONDS
     PRINT *, ""
     
-15      FORMAT(/'Calculation time is ', F12.6, ' seconds.')
+15      FORMAT(/'Calculation time is', F6.3, ' seconds.')
+    !PRINT *, "Elapsed time:", ELAPSED_SECONDS, "seconds"
 
     PRINT *, "Top left corner of matrix A:"
     PRINT 20, ((A(I,J), J = 1,MIN(K,6)), I = 1,MIN(M,6))
