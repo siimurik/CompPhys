cc92ec2417584f94cb63e93e229af7aa67fa8af6
siimurik <82040815+siimurik@users.noreply.github.com>
2023-08-14 00:39:22 +0300

    Add files via upload

--- Adv_C_Fortran/C_Advanced/MatMat/GSL_Method/gsl_matmul.c
+++ Adv_C_Fortran/C_Advanced/MatMat/GSL_Method/gsl_matmul.c
@@ -1,10 +1,13 @@
 /*
+==============================================================================
+Best possible command for fast results:
+    $ gcc -o gsl_openblas gsl_matmul.c -lgsl -lopenblas -lpthread -lm
 Help with compiling commands
     $ gcc -I/usr/include/gsl -lgsl -lgslcblas -lm gsl_matmul.c -o gsl_matmul
     $ ./a.out
     // Result of 5000x5000 multiplication
       time = 669.75406 sec. 
-                            
+==============================================================================                            
 */
 
 #include <time.h>
@@ -13,8 +16,10 @@ Help with compiling commands
 
 int
 main (void)
-{
-    int n = 2000;
+{   
+    struct timespec start, stop;
+    int n = 5000;
+
     printf ("Dimension of matrices: %d x %d.\n", n, n);
     int size = n*n;
     double *aq = malloc (sizeof (double) * size);
@@ -34,14 +39,26 @@ main (void)
     gsl_matrix_view B = gsl_matrix_view_array(bq, n, n);
     gsl_matrix_view C = gsl_matrix_view_array(cq, n, n);
 
+    // Set the number of threads for OpenBLAS
+    // Actually not even necessary to explicidly write
+    //openblas_set_num_threads(8); // Set the desired number of threads
+
     /* Compute C = A B */
-    clock_t begin = clock();
+    
+    // Get starting time
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,
                   1.0, &A.matrix, &B.matrix,
                   0.0, &C.matrix);
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+    // Get end time
+    clock_gettime(CLOCK_MONOTONIC, &stop);
 
-    printf (" time = %.8g sec.\n", time_spent);
+    // Calculate the elapsed time in seconds
+    double time_taken = (stop.tv_sec - start.tv_sec) * 1e9;
+    time_taken = (time_taken + (stop.tv_nsec - start.tv_nsec)) * 1e-9;
+    printf("\nMatrix multiplication took %lf seconds.\n", time_taken);
+
     return 0;
 }
